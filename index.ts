bd1ad0158e9829a286a7364a147a034110488c39
RM-Souza <rafael.sovza@gmail.com>
2022-08-19 19:36:08 -0300

    testing typescript

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log('Hey!');
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
